e52689bebd46fac6dc5e0ee54475e940cb71dc19
Magsamm <19634056+Magsamm@users.noreply.github.com>
2024-07-26 21:38:53 +0200

    removed commented out function and some comments

--- script.js
+++ script.js
@@ -5,15 +5,8 @@ btn.setAttribute("id", "btn");
 btn.textContent = "16x16 grid";
 document.body.appendChild(btn);
 
-// rowQuery.addEventListener("mouseover", () => {
-//     // Change the button's background color
-//     rowQuery.style.backgroundColor = "blue";
-// });
-
 //add click event for button to create a grid of divs
 btn.addEventListener("click", () => {
-    //change to 16x16 after testing
-    //use nested loop to create rows and columns?
     for (let i = 0; i < 16; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
