2ba8697160555484ff498efc7a94431c59544146
Magsamm <19634056+Magsamm@users.noreply.github.com>
2024-07-26 19:52:16 +0200

    basic etch a sketch is up. step 3 completed.

---
+++ script.js
@@ -0,0 +1,33 @@
+const container = document.querySelector("#container");
+
+const btn = document.createElement("button");
+btn.setAttribute("id", "btn");
+btn.textContent = "16x16 grid";
+document.body.appendChild(btn);
+
+// rowQuery.addEventListener("mouseover", () => {
+//     // Change the button's background color
+//     rowQuery.style.backgroundColor = "blue";
+// });
+
+//add click event for button to create a grid of divs
+btn.addEventListener("click", () => {
+    //change to 16x16 after testing
+    //use nested loop to create rows and columns?
+    for (let i = 0; i < 128; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for (let j = 0; j < 128; j++) {
+            const column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+        }
+    }
+    const rowQuery = document.querySelectorAll("div.column");
+    for (const element of rowQuery) {
+        element.addEventListener("mouseover", (e) => {
+            e.target.style.background = "blue";
+        });
+    }
+});
