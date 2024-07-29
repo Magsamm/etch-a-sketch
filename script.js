32a93e1aabf381a82d3a833fc62622455cdb9a9f
Magsamm <19634056+Magsamm@users.noreply.github.com>
2024-07-29 23:11:57 +0200

    attempt to add a way to clear grid on button press

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
-const container = document.querySelector("#container");
+const container = document.querySelector(".container");
 
 const btn = document.createElement("button");
 btn.setAttribute("id", "btn");
-btn.textContent = "16x16 grid";
+btn.textContent = "Enter number of cells you want for the grid";
 document.body.appendChild(btn);
 
+//store input from user to use in the click event to generate a grid of x squares
+let gridValue = 0;
+
+function getHumanChoice() {
+    userInput = parseInt(prompt("Enter a number between 1 and 100"));
+    //run loop until user inputs a valid choice.
+    while (userInput < 1 || userInput > 100)
+        userInput = parseInt(
+            prompt("Please enter a number thats more than 1 and lower than 100")
+        );
+    gridValue += userInput;
+    return userInput;
+}
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 //add click event for button to create a grid of divs
+//change this button to send a prompt
+//make sure it uses the same number of pixels regardless of grid size specified
 btn.addEventListener("click", () => {
-    for (let i = 0; i < 16; i++) {
+    const removeChildsContainer = document.querySelector(".container");
+    removeAllChildNodes(removeChildsContainer);
+
+    //remember to wipe existing grid when user inputs grid
+    container.textContent = "";
+
+    getHumanChoice();
+
+    for (let i = 0; i < gridValue; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
-        container.appendChild(row);
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < gridValue; j++) {
             const cell = document.createElement("div");
             cell.classList.add("cell");
             row.appendChild(cell);
+            container.appendChild(row);
         }
     }
+
     //use math.floor random to generate random color?
     const cellColor = document.querySelectorAll("div.cell");
     for (const element of cellColor) {
@@ -25,3 +55,5 @@ btn.addEventListener("click", () => {
         });
     }
 });
+
+//try using a foreach and loop over
